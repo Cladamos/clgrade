3b48f9424b580612f5ed9d64595d5457057ef862
cladamos <caliskangokberk@gmail.com>
2026-08-19 20:10:27 +0300

    feat: load the image in a separate thread

--- src/app.rs
+++ src/app.rs
@@ -1,18 +1,25 @@
 use std::io;
+use std::sync::mpsc;
+use std::thread;
 
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
 use ratatui::{
     DefaultTerminal, Frame,
     buffer::Buffer,
     crossterm,
-    layout::{Rect, Size},
-    widgets::{FrameExt, Widget},
+    layout::{Constraint, Margin, Rect, Size},
+    style::{Color, Stylize},
+    text::Text,
+    widgets::{Block, BorderType::Rounded, Borders, FrameExt, Widget},
 };
 use ratatui_explorer::{FileExplorer, FileExplorerBuilder, Theme};
 use ratatui_image::{FilterType::Nearest, Image, Resize, picker::Picker, protocol::Protocol};
 
 pub struct App {
     image: Option<Protocol>,
+    image_rx: mpsc::Receiver<Protocol>,
+    image_tx: mpsc::Sender<Protocol>,
+    loading: bool,
     is_file_explorer_visible: bool,
     is_image_selected: bool,
     file_explorer: FileExplorer,
@@ -23,6 +30,7 @@ const SUPPORTED_FORMATS: &[&str] = &["png", "jpg", "jpeg", "webp"];
 
 impl App {
     pub fn new() -> Self {
+        let (image_tx, image_rx) = mpsc::channel();
         let theme = Theme::default().add_default_title();
         let mut file_explorer = FileExplorerBuilder::build_with_theme(theme).unwrap();
         file_explorer
@@ -40,11 +48,14 @@ impl App {
             .unwrap();
 
         App {
+            image: None,
+            image_rx: image_rx,
+            image_tx: image_tx,
+            loading: false,
             exit: false,
             is_file_explorer_visible: false,
             is_image_selected: false,
             file_explorer: file_explorer,
-            image: None,
         }
     }
 
@@ -52,57 +63,52 @@ impl App {
         while !self.exit {
             terminal.draw(|frame| self.draw(frame))?;
             self.handle_events()?;
-
-            let picker = Picker::from_query_stdio().unwrap();
-
-            if self.is_image_selected
-                && let Some(img_path) = self.file_explorer.current().path.to_str()
-            {
-                if img_path.ends_with(".png")
-                    || img_path.ends_with(".jpg")
-                    || img_path.ends_with(".jpeg")
-                    || img_path.ends_with(".webp")
-                {
-                    let dyn_img = image::ImageReader::open(img_path)
-                        .unwrap()
-                        .decode()
-                        .unwrap();
-
-                    let target_size = Size::new(40, 40);
-
-                    self.image = Some(
-                        picker
-                            .new_protocol(dyn_img, target_size, Resize::Fit(Some(Nearest)))
-                            .unwrap(),
-                    );
-
-                    self.is_image_selected = false;
-                }
+            if self.is_image_selected {
+                self.draw_image();
+            }
+            if let Ok(protocol) = self.image_rx.try_recv() {
+                self.image = Some(protocol);
+                self.loading = false;
             }
         }
         Ok(())
     }
 
-    fn draw(&self, frame: &mut Frame) {
-        if self.is_file_explorer_visible {
-            frame.render_widget_ref(self.file_explorer.widget(), frame.area());
-        } else {
-            frame.render_widget(self, frame.area());
-        }
+    fn draw_image(&mut self) {
+        let tx = self.image_tx.clone();
+        let picker = Picker::from_query_stdio().unwrap();
+        let img_path = self.file_explorer.current().path.clone();
+        self.loading = true;
+        thread::spawn(move || {
+            let dyn_img = image::ImageReader::open(img_path)
+                .unwrap()
+                .decode()
+                .unwrap();
+
+            let target_size = Size::new(60, 30);
+
+            let image_protocol = picker
+                .new_protocol(dyn_img, target_size, Resize::Fit(Some(Nearest)))
+                .unwrap();
+
+            tx.send(image_protocol).unwrap();
+        });
+        self.is_image_selected = false;
     }
 
-    /// updates the application's state based on user input
     fn handle_events(&mut self) -> io::Result<()> {
-        let event = event::read()?;
-        match event {
-            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
-                self.handle_key_event(key_event)
-            }
-            _ => {}
-        };
+        if event::poll(std::time::Duration::from_millis(16))? {
+            let event = event::read()?;
+            match event {
+                Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                    self.handle_key_event(key_event)
+                }
+                _ => {}
+            };
 
-        if self.is_file_explorer_visible {
-            self.file_explorer.handle(&event)?;
+            if self.is_file_explorer_visible {
+                self.file_explorer.handle(&event)?;
+            }
         }
         Ok(())
     }
@@ -112,12 +118,19 @@ impl App {
             KeyCode::Char('q') => self.exit(),
             KeyCode::Char('f') => self.is_file_explorer_visible = !self.is_file_explorer_visible,
             KeyCode::Enter => {
-                if self.is_file_explorer_visible == true {
+                if self.is_file_explorer_visible && self.file_explorer.current().is_dir == false {
                     self.is_image_selected = true;
                     self.is_file_explorer_visible = false;
                 }
             }
             _ => {}
+        }
+    }
+    fn draw(&self, frame: &mut Frame) {
+        if self.is_file_explorer_visible {
+            frame.render_widget_ref(self.file_explorer.widget(), frame.area());
+        } else {
+            frame.render_widget(self, frame.area());
         }
     }
     fn exit(&mut self) {
@@ -127,8 +140,41 @@ impl App {
 
 impl Widget for &App {
     fn render(self, area: Rect, buf: &mut Buffer) {
+        let image_area = Rect {
+            x: area.x,
+            y: area.y,
+            width: 62,
+            height: 32,
+        };
+
         if let Some(image) = self.image.as_ref() {
-            Image::new(image).allow_clipping(true).render(area, buf);
+            Image::new(image).allow_clipping(true).render(
+                image_area.inner(Margin::new(1, 1)).centered(
+                    Constraint::Length(image.size().width),
+                    Constraint::Length(image.size().height),
+                ),
+                buf,
+            );
         }
+        if self.image.is_none() && !self.loading {
+            Text::from("Select an image with 'f' to open file explorer.")
+                .fg(Color::DarkGray)
+                .render(
+                    image_area.centered(Constraint::Length(47), Constraint::Length(1)),
+                    buf,
+                );
+        }
+        // TODO: add loading animation, instead of plain text.
+        if self.loading {
+            Text::from("Loading...").fg(Color::DarkGray).render(
+                image_area.centered(Constraint::Length(10), Constraint::Length(1)),
+                buf,
+            );
+        }
+        Block::default()
+            .title("Image")
+            .borders(Borders::ALL)
+            .border_type(Rounded)
+            .render(image_area, buf);
     }
 }
